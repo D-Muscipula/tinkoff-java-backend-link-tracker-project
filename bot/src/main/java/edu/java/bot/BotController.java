2346c9e130cb6804e858e1d2ef38d6ef58369e55
Sergey K <s4-7@yandex.ru>
2024-03-03 21:49:32 +0300

    bot controller

---
+++ bot/src/main/java/edu/java/bot/BotController.java
@@ -0,0 +1,23 @@
+package edu.java.bot;
+
+import edu.java.bot.dto.LinkUpdate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BotController {
+    private final Logger logger = LoggerFactory.getLogger(BotController.class);
+
+    @PostMapping("/updates")
+    public ResponseEntity<Void> sendUpdates(
+        @RequestBody LinkUpdate linkUpdate
+    ) {
+        logger.info("обновления получены");
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
--- bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
+++ bot/src/main/java/edu/java/bot/configuration/ApplicationConfig.java
@@ -1,11 +1,15 @@
 package edu.java.bot.configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import jakarta.validation.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
+@OpenAPIDefinition(info = @Info(title = "Bot API",
+                                description = "Bot API", version = "1.0.0"))
 public record ApplicationConfig(
     @NotEmpty
     String telegramToken
