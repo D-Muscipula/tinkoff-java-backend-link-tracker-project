b3d97749e24edb216656aa969ce1f07210a08daf
Sergey K <s4-7@yandex.ru>
2024-03-09 11:30:05 +0300

    fix scheduler

--- scrapper/src/main/java/edu/java/scheduling/LinkUpdaterScheduler.java
+++ scrapper/src/main/java/edu/java/scheduling/LinkUpdaterScheduler.java
@@ -7,7 +7,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 public class LinkUpdaterScheduler {
     private final Logger logger = LoggerFactory.getLogger(LinkUpdaterScheduler.class);
 
-    @Scheduled(fixedDelayString = "#{clientConfiguration.applicationConfig.scheduler().interval()}")
+    @Scheduled(fixedDelayString = "#{scheduler.interval()}")
     public void update() {
         logger.info("it works");
     }
