c2d37e23185c17a3fe7995f8ca3d67f15137765d
Sergey K <s4-7@yandex.ru>
2024-03-14 19:22:23 +0300

    small change db

--- migrations/1_create_tables.sql
+++ migrations/1_create_tables.sql
@@ -3,11 +3,10 @@
 
 CREATE TABLE IF NOT EXISTS users
 (
-    id           BIGINT GENERATED ALWAYS AS IDENTITY,
     user_chat_id BIGINT NOT NULL,
     user_state   TEXT   NOT NULL,
 
-    PRIMARY KEY (id),
+    PRIMARY KEY (user_chat_id),
     UNIQUE (user_chat_id)
 );
 
@@ -17,6 +16,7 @@ CREATE TABLE IF NOT EXISTS links
     url        VARCHAR                  NOT NULL,
 
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    last_checked_at TIMESTAMP WITH TIME ZONE,
 
     PRIMARY KEY (id),
     UNIQUE (url)
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS users_links
     user_id BIGINT,
     link_id BIGINT NOT NULL,
 
-    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (user_id) REFERENCES users (user_chat_id),
     FOREIGN KEY (link_id) REFERENCES links (id),
     PRIMARY KEY (user_id, link_id)
 );
