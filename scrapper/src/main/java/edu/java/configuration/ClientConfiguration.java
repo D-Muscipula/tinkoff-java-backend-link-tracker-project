948ba182b0d7a6bea7df889f210a18f19338ab5b
Sergey K <s4-7@yandex.ru>
2024-03-17 16:24:41 +0300

    fix

--- scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
+++ scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
@@ -3,7 +3,6 @@ package edu.java.configuration;
 import edu.java.client.BotClient;
 import edu.java.client.GitHubClient;
 import edu.java.client.StackOverflowClient;
-import edu.java.scheduling.LinkUpdaterScheduler;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
