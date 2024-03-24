ac6b808a3dc8449c855f846b420c31870efa9b0c
Sergey K <s4-7@yandex.ru>
2024-03-24 22:25:46 +0300

    fix

--- scrapper/src/main/java/edu/java/scrapper/domain/repository/jpa/JpaUserRepository.java
+++ scrapper/src/main/java/edu/java/scrapper/domain/repository/jpa/JpaUserRepository.java
@@ -2,7 +2,6 @@ package edu.java.scrapper.domain.repository.jpa;
 
 import edu.java.scrapper.domain.jpa.TgUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
 public interface JpaUserRepository extends JpaRepository<TgUser, Long> {
