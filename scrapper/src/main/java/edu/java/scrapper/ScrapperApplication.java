846830883e3b5948f62ca2f7b435a685210ca356
Sergey K <s4-7@yandex.ru>
2024-03-31 23:55:55 +0300

    bucket4j

--- scrapper/src/main/java/edu/java/scrapper/ScrapperApplication.java
+++ scrapper/src/main/java/edu/java/scrapper/ScrapperApplication.java
@@ -6,11 +6,13 @@ import edu.java.scrapper.configuration.RetryConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableConfigurationProperties({ApplicationConfig.class, RetryConfig.class})
+@EnableCaching
 public class ScrapperApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScrapperApplication.class, args);
