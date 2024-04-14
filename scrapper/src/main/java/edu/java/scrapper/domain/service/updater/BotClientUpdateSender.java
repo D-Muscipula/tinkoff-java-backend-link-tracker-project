15a105c82341b3a25d5559f9c04bb08a92ab9c9e
Sergey K <s4-7@yandex.ru>
2024-04-15 00:40:05 +0300

    add new metric

--- scrapper/src/main/java/edu/java/scrapper/domain/service/updater/BotClientUpdateSender.java
+++ scrapper/src/main/java/edu/java/scrapper/domain/service/updater/BotClientUpdateSender.java
@@ -2,6 +2,8 @@ package edu.java.scrapper.domain.service.updater;
 
 import dto.request.LinkUpdate;
 import edu.java.scrapper.client.BotClient;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
@@ -10,14 +12,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class BotClientUpdateSender implements UpdateSender {
     private final BotClient botClient;
+    private final Counter updatesCounter;
 
     @Autowired
-    public BotClientUpdateSender(BotClient botClient) {
+    public BotClientUpdateSender(BotClient botClient, MeterRegistry meterRegistry) {
         this.botClient = botClient;
+        updatesCounter = meterRegistry.counter("count_of_update_sends");
     }
 
     @Override
     public void sendUpdate(LinkUpdate linkUpdate) {
+        updatesCounter.increment();
         botClient.sendUpdate(linkUpdate);
     }
 }
--- scrapper/src/main/java/edu/java/scrapper/domain/service/updater/KafkaUpdateSender.java
+++ scrapper/src/main/java/edu/java/scrapper/domain/service/updater/KafkaUpdateSender.java
@@ -2,6 +2,8 @@ package edu.java.scrapper.domain.service.updater;
 
 import dto.request.LinkUpdate;
 import edu.java.scrapper.domain.service.kafka_queue_producer.ScrapperQueueProducer;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
@@ -10,14 +12,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaUpdateSender implements UpdateSender {
     private final ScrapperQueueProducer scrapperQueueProducer;
+    private final Counter updatesCounter;
 
     @Autowired
-    public KafkaUpdateSender(ScrapperQueueProducer scrapperQueueProducer) {
+    public KafkaUpdateSender(ScrapperQueueProducer scrapperQueueProducer, MeterRegistry meterRegistry) {
+        updatesCounter = meterRegistry.counter("count_of_update_sends");
         this.scrapperQueueProducer = scrapperQueueProducer;
     }
 
     @Override
     public void sendUpdate(LinkUpdate linkUpdate) {
+        updatesCounter.increment();
         scrapperQueueProducer.send(linkUpdate);
     }
 }
